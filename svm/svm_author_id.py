bdf7131c00b31369f3ed09aa158c494b09312e36
Amrinder singh <amrinder.singh@sap.com>
2017-02-07 22:53:06 +0530

    svm mini project completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+
+clf = SVC(kernel='linear')
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print "Accuracy:"
+print accuracy_score(pred, labels_test)
 #########################################################
 
 
