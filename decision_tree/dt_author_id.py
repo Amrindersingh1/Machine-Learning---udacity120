8dd4a69b3f1f7f6f0a456244342a34c90bd802b5
Amrinder singh <amrinder.singh@sap.com>
2017-02-08 01:28:24 +0530

    completed decision tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,19 +12,32 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn import tree
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time fit:", round(time()-t0, 3), "s"
 
 
 
+t0 = time()
+pred = clf.predict(features_test)
+print "training time predict:", round(time()-t0, 3), "s"
 #########################################################
 ### your code goes here ###
-
+print "Accuracy:"
+print accuracy_score(pred, labels_test)
 
 #########################################################
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,12 +26,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+import numpy as np
 
+clf = SVC(kernel='rbf', C=10000.0)
 
-clf = SVC(kernel='linear')
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time fit:", round(time()-t0, 3), "s"
 
+
+
+t0 = time()
 pred = clf.predict(features_test)
+print "training time predict:", round(time()-t0, 3), "s"
+
+print "perdictions:"
+print "perdictions for 10 - "
+print pred[10]
+print "perdictions for 26 - "
+print pred[26]
+print "perdictions for 50 - "
+print pred[50]
+
+print "perdictions in favour of 1:"
+print np.count_nonzero(pred == 1)
 
 print "Accuracy:"
 print accuracy_score(pred, labels_test)
